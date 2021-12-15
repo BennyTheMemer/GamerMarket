8a1cdddcdc7c64983bccb228e4f0616454c7ee18
BennyTheMemer <bernardolcc98@gmail.com>
2021-12-15 15:59:16 +0000

    Auth, user session

--- src/components/SignupModal.js
+++ src/components/SignupModal.js
@@ -24,12 +24,112 @@ import {
   ModalCloseButton,
   useDisclosure,
 } from "@chakra-ui/react";
+import { isEmail } from "validator";
+import AuthService from "../services/authservice";
+import Form from "react-validation/build/form";
+import CheckButton from "react-validation/build/button";
+import { TriangleUpIcon } from "@chakra-ui/icons";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+const email = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
+
+const vusername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The username must be between 3 and 20 characters.
+      </div>
+    );
+  }
+};
+
+const vpassword = (value) => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+};
 
 export default function SignUp() {
   const [isOpen, setOpen] = useState(false);
   const [registerIsOpen, setRegisterOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [message, setMessage] = useState("");
+  const [successful, setSuccessful] = useState(true);
+  const form = React.useRef();
+  const checkBtn = React.useRef();
+
   const initialRef = React.useRef();
   const finalRef = React.useRef();
+
+  function onChangeUsername(e) {
+    setUsername(e.target.value);
+    console.log(username);
+  }
+
+  function onChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function onChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function onChangePassword2(e) {
+    setPassword2(e.target.value);
+  }
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setMessage("");
+    setSuccessful(false);
+
+    form.current.validateAll();
+
+    if (checkBtn.current.context._errors.length === 0) {
+      AuthService.register(username, email, password).then(
+        (response) => {
+          setMessage(response.data.message);
+          setSuccessful(TriangleUpIcon);
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          setMessage(resMessage);
+          setSuccessful(false);
+        }
+      );
+    }
+  }
 
   function changeRegister() {
     onClose();
@@ -73,44 +173,80 @@ export default function SignUp() {
             <ModalHeader>Register</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <Input
-                border="0px"
-                borderRight="1px"
-                borderBottom="1px"
-                focusBorderColor="white"
-                ref={initialRef}
-                type="username"
-                placeholder="username"
-                mb="5%"
-              />
-              <Input
-                border="0px"
-                borderRight="1px"
-                borderBottom="1px"
-                focusBorderColor="white"
-                type="email"
-                placeholder="email"
-                mb="5%"
-              />
-              <Input
-                border="0px"
-                borderRight="1px"
-                borderBottom="1px"
-                focusBorderColor="white"
-                type="password"
-                placeholder="password"
-                mb="5%"
-              />
-              <Input
-                border="0px"
-                borderRight="1px"
-                borderBottom="1px"
-                focusBorderColor="white"
-                type="password"
-                mb="5%"
-                placeholder="confirm password"
-                shadow="0px"
-              />
+              <Form onSubmit={handleRegister} ref={form}>
+                {successful && (
+                  <div>
+                    <div className="form-group">
+                      <label htmlFor="username">Username</label>
+                      <Input
+                        type="text"
+                        className="form-control"
+                        name="username"
+                        value={username}
+                        onChange={onChangeUsername}
+                        validations={[required, vusername]}
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="email">Email</label>
+                      <Input
+                        type="text"
+                        className="form-control"
+                        name="email"
+                        value={email}
+                        onChange={onChangeEmail}
+                        validations={[required, email]}
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <Input
+                        type="password"
+                        className="form-control"
+                        name="password"
+                        value={password}
+                        onChange={onChangePassword}
+                        validations={[required, vpassword]}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="password">Confirm Password</label>
+                      <Input
+                        type="password"
+                        className="form-control"
+                        name="password2"
+                        value={password2}
+                        onChange={onChangePassword2}
+                        validations={[required, vpassword]}
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <button className="btn btn-primary btn-block">
+                        Sign Up
+                      </button>
+                    </div>
+                  </div>
+                )}
+
+                {message && (
+                  <div className="form-group">
+                    <div
+                      className={
+                        successful
+                          ? "alert alert-success"
+                          : "alert alert-danger"
+                      }
+                      role="alert"
+                    >
+                      {message}
+                    </div>
+                  </div>
+                )}
+                <CheckButton style={{ display: "none" }} ref={checkBtn} />
+              </Form>
             </ModalBody>
 
             <ModalFooter>
@@ -123,7 +259,7 @@ export default function SignUp() {
               >
                 <Text textDecoration="underline">Já tens conta?</Text>
               </Button>
-              <Button onClick={useEffect} variant="gamer">
+              <Button type="submit" onClick={handleRegister} variant="gamer">
                 Register
               </Button>
             </ModalFooter>
